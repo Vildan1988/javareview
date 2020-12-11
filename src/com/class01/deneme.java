ded6c54e4900eea855150a2c51dbf6bbac4e8cb9
Vildan Sert <vildansert@Vildans-MBP15.home>
2020-12-11 12:23:12 -0600

    java review clases

---
+++ src/com/class01/deneme.java
@@ -0,0 +1,12 @@
+package com.class01;
+
+public class deneme {
+public static void main (String args []) {
+	
+	System.out.println("Selam Vildancigim lutfen bunlari ezberleyelim");
+	
+	int a=1 ;
+	int b=2;
+	System.out.println(a+b);
+}
+}
